aff0f76c42cd5d134066610c8327b1c0cd137cb3
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-13 15:37:45 +0530

    chore: add func to check number is prime or amstrong

---
+++ checkPrimeAmstrong.js
@@ -0,0 +1,29 @@
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  let c = 0;
+  for (let i = 1; i < Math.sqrt(n); i++) {
+    if (n % i == 0) {
+      c++;
+    }
+  }
+  if (c == 2) {
+    return true;
+  }
+  return false;
+}
+
+function isAmstrong(n) {
+  let str = n.toString();
+  let l = str.length;
+  let s = 0;
+  while (n > 0) {
+    s += Math.pow(n % 10, l);
+    n /= 10;
+  }
+  if (s == n) {
+    return true;
+  }
+  return false;
+}
