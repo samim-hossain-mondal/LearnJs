226f49766d3c75a35a9845788b27456cc05d508f
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-13 16:02:24 +0530

    feat: add arrow function to check prime and amstrong

--- checkPrimeAmstrong.js
+++ checkPrimeAmstrong.js
@@ -4,7 +4,7 @@ function isPrime(n) {
   }
   let c = 0;
   for (let i = 1; i <= n; i++) {
-    if (n % i == 0) {
+    if (n % i === 0) {
       c++;
     }
   }
@@ -14,20 +14,53 @@ function isPrime(n) {
   return false;
 }
 
+let prime = (n) => {
+  if (n < 2) {
+    return false;
+  }
+  let c = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      c++;
+    }
+  }
+  if (c == 2) {
+    return true;
+  }
+  return false;
+};
+
 function isAmstrong(n) {
   let str = n.toString();
   let l = str.length;
+  let num = n;
   let s = 0;
-  while (n > 0) {
-    s += Math.pow(n % 10, l);
-    n /= 10;
+  while (num > 0) {
+    s += parseInt(Math.pow(num % 10, l));
+    num = parseInt(num / 10);
   }
-  if (s == n) {
+  if (s === n) {
     return true;
   }
   return false;
 }
 
+let amstrong = (n) => {
+  let str = n.toString();
+  let l = str.length;
+  let num = n;
+  let s = 0;
+  while (num > 0) {
+    s += parseInt(Math.pow(num % 10, l));
+    num = parseInt(num / 10);
+  }
+  if (s === n) {
+    return true;
+  }
+  return false;
+};
+
 console.log(isPrime(23));
-console.log(isPrime(21));
-console.log(isAmstrong(9474));
+console.log(prime(21));
+console.log(amstrong(9474));
+console.log(isAmstrong(123));
