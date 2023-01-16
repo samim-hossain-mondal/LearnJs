ea2aab27ab60fce8ebb8864ed3241e2047c98cae
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-16 22:39:02 +0530

    feat: add functionality to filter all even numbers from array

---
+++ evenNumberFiltering.js
@@ -0,0 +1,8 @@
+const evenNumberInArray = (numbers) => {
+    if (!Array.isArray(numbers) || numbers.length == 0) {
+        return new Error('Invalid input!');
+    }
+    return numbers.filter(item => item % 2 == 0);
+};
+
+console.log(evenNumberInArray([1, 2, 3, 4, 5, 6, 7]));
