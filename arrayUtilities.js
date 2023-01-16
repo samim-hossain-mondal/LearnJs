053c398c3f0e11b37e91343b7895f66c621c8de0
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-16 22:36:18 +0530

    feat: add functionality to double the elements in array

---
+++ arrayUtilities.js
@@ -0,0 +1,9 @@
+const doubleNumbers = (numbers) => {
+    if (!Array.isArray(numbers) || numbers.length == 0) {
+        throw new Error('Invalid input!');
+    }
+    return numbers.map(item => item * 2);
+};
+
+//console.log(doubleNumbers([]));
+module.exports = { doubleNumbers };
