30204a2ac91885088d275795a38d0284404165fc
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-17 09:23:54 +0530

    feat: add functionality to filter and triple even numbers in array

--- evenNumberTripling.js
+++ evenNumberTripling.js
@@ -6,9 +6,17 @@ const getTripleOfEvenNumbersFM = (numbers) => {
     numbers = numbers.filter(item => item % 2 == 0);
     return numbers.map(item => item * 3);
 };
-console.log(getTripleOfEvenNumbersFM([1, 2, 3, 4, 5, 6, 7, 8]));
+//console.log(getTripleOfEvenNumbersFM([1, 2, 3, 4, 5, 6, 7, 8]));
 
 // Using reduce
 const getTripleOfEvenNumbersR = (numbers) => {
-
+    return numbers.reduce((acc, cuu) => {
+        if (cuu % 2 === 0) {
+            acc.push(cuu * 3);
+        }
+        return acc;
+    }, []);
 };
+//console.log(getTripleOfEvenNumbersR([1, 2, 3, 4, 5, 6, 7, 8]));
+
+module.exports = { getTripleOfEvenNumbersFM, getTripleOfEvenNumbersR };
